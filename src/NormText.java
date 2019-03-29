b52dc3c68f6636297510939e4baea61456e755c6
Corvin13 <Corvin_13@inbox.ru>
2019-03-29 17:50:50 +0400

    Initial commit

---
+++ src/NormText.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class NormText {
+
+    public static Map<Integer, String> map = new HashMap<>();
+
+    static {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2, "два");
+        map.put(3, "три");
+        map.put(4, "четыре");
+        map.put(5, "пять");
+        map.put(6, "шесть");
+        map.put(7, "семь");
+        map.put(8, "восемь");
+        map.put(9, "девять");
+        map.put(10, "десять");
+        map.put(11, "одиннадцать");
+        map.put(12, "двенадцать");
+        map.put(13, "тринадцать");
+        map.put(14, "четырнадцать");
+        map.put(15, "пятнадцать");
+        map.put(16, "шестнадцать");
+        map.put(17, "семнадцать");
+        map.put(18, "восемнадцать");
+
+    }
+}
