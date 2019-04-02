8aa7d0b56c421c58b83851bfceec087778f6d68f
manunia <timoshenkomaria@mail.ru>
2019-04-02 20:11:31 +0300

    морфер больше не используется добавлены методы проверки на дату и на время и методы преобразования даты и времени в пропись

--- src/NormText.java
+++ src/NormText.java
@@ -1,22 +1,21 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import com.ibm.icu.text.RuleBasedNumberFormat;
+import com.ibm.icu.util.ULocale;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-import ru.morpher.ws3.*;
-import ru.morpher.ws3.russian.ArgumentNotRussianException;
-import ru.morpher.ws3.russian.NumberSpellingResult;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NormText {
 
     public static Map<Integer, String> map = new HashMap<Integer, String>();
 
     static {
-        map.put(0, "ноль");
         map.put(1, "один");
         map.put(2, "два");
         map.put(3, "три");
@@ -40,78 +39,111 @@ public class NormText {
         map.put(21, "двадцать один");
         map.put(22, "двадцать два");
         map.put(23, "двадцать три");
-        map.put(24, "двадцать четыре");
-        map.put(25, "двадцать пять");
-        map.put(26, "двадцать шесть");
-        map.put(27, "двадцать семь");
-        map.put(28, "двадцать восемь");
-        map.put(29, "двадцать девять");
-        map.put(30, "тридцать");
-        map.put(31, "тридцать один");
     }
 
     public static void main(String[] args) {
-
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String fileName = reader.readLine();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
             String line;
-            while ((line = bufferedReader.readLine()) != null) System.out.println(replace(line));
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(replace(line));
+            }
             reader.close();
             bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
     private static String replace(String line) {
-        Client client = new ClientBuilder().build();
 
         StringBuilder builder = new StringBuilder();
         StringTokenizer st = new StringTokenizer(line, " ");
 
         while(st.hasMoreTokens()) {
             String line1 = st.nextToken();
-            if (line1.equals("00")) {
-                line1 = "ноль ноль";
+            //System.out.println(line1);
+            if (isTime(line1)){
+                line1 = transTime(line1);
             }
-            try {
-                int x = Integer.parseInt(line1);
-
-                try {
-                    NumberSpellingResult result = client.russian().spell(x, "дата");
-
-                    line1 = result.numberDeclension.nominative;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (ArgumentNotRussianException e) {
-                    e.printStackTrace();
-                } catch (ArgumentEmptyException e) {
-                    e.printStackTrace();
-                } catch (AccessDeniedException e) {
-                    e.printStackTrace();
-                }
-
-
-//                if (x < 30) {
-//                    for (Map.Entry<Integer, String> entry : map.entrySet()) {
-//                        if (x == entry.getKey())
-//                            line1 = entry.getValue();
-//                    }
-//                } else if (x > 1999){
-//                    for (Map.Entry<Integer, String> entry : map.entrySet()) {
-//                        if ((x - 2000) == entry.getKey())
-//                            line1 = "две тысячи " + entry.getValue();
-//
-//                    }
-//                }
-            } catch (NumberFormatException e) {
+            if (isDate(line1)) {
+                line1 = transDate(line1);
+                line1 = prepareDate(line1);
             }
             builder.append(line1);
             builder.append(" ");
         }
         return builder.toString().trim();
+    }
+
+    private static boolean isTime(String line) {
+        String[] str = line.split("[:]");
+        try {
+            int x = Integer.parseInt(str[0]);
+            if (x>=0 && x <= 23) return true;
+            else return false;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDate(String line) {
+        String pattern = "(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String transDate(String line) {
+        String[] strings = line.split("[.]");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMM yyyy");
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(strings[0]));
+        calendar.set(Calendar.MONTH, Integer.parseInt(strings[1]) - 1);
+        calendar.set(Calendar.YEAR, Integer.parseInt(strings[2]));
+
+        line = dateFormat.format(calendar.getTime());
+        return line;
+    }
+
+    private static String prepareDate(String line) throws NumberFormatException {
+        String[] strings = line.split(" ");
+        RuleBasedNumberFormat format = new RuleBasedNumberFormat(new ULocale("ru"), RuleBasedNumberFormat.SPELLOUT);
+        String ruleset = "%spellout-ordinal-neuter-genitive";
+        line = format.format(Integer.parseInt(strings[0]), ruleset) + " " +
+                strings[1] + " " + format.format(Integer.parseInt(strings[2]),ruleset) + " года";
+        return line;
+    }
+
+    private static String transTime(String line) {
+        String[] strings = line.split("[:]");
+        line = "";
+        for (int i = 0; i < strings.length; i++) {
+
+            if (strings[i].equals("00")) {
+                strings[i] = " ноль ноль";
+                line+=strings[i];
+            }
+            try {
+                int x = Integer.parseInt(strings[i]);
+
+                for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                    if (x == entry.getKey()) {
+                        strings[i] = entry.getValue();
+                        line+=strings[i];
+                    }
+                }
+            } catch (NumberFormatException e) {
+            }
+
+        }
+        return line;
+
     }
 
 }
